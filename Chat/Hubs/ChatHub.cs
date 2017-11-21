168a22e26a97cfa52f88da714b8967d76f9e9fe5
Unknown <pavel1yakimovich@gmail.com>
2017-11-21 11:32:30 +0300

    added methods for connecting and triggering disconnection of users

---
+++ Chat/Hubs/ChatHub.cs
@@ -0,0 +1,40 @@
+﻿using Chat.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+
+namespace Chat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private static List<User> users = new List<User>();
+
+        public void Connect(string userName)
+        {
+            var id = Context.ConnectionId;
+
+            if (!users.Any(u => u.ConnectionId == id))
+            {
+                users.Add(new User { ConnectionId = id, UserName = userName });
+
+                Clients.Caller.onConnected(id, userName, users);
+
+                Clients.AllExcept(id).onNewUserConnected(id, userName);
+            }
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            var connectionId = Context.ConnectionId;
+            var item = users.FirstOrDefault(u => u.ConnectionId == connectionId);
+
+            if (users.Remove(item))
+            {
+                Clients.All.onUserDisconnected(connectionId, item.UserName);
+            }
+
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
